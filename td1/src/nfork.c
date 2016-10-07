c965214b8779ebcc023af5116f1c38b9485b683d
Arib Yassine <aribyassine@hotmail.fr>
2016-10-07 16:53:52 +0200

    chaine proc

--- td1/src/nfork.c
+++ td1/src/nfork.c
@@ -1,38 +1,17 @@
-#include <unistd.h>
-#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int nfork(int nb_fils){
-	pid_t pid;
-	int i = 0;
-	while(i<nb_fils){
-	pid = fork();
-		switch (pid) {
-		case -1:
-			return -1;
-		break;
-		/* Si on est dans le fils */
-		case 0:
-			return 0;
-		break;
-		/* Si on est dans le père */
-		default:
-			i++;
-		break;
-		}
-	}
-	return i;
-}
-
-int main (void) {
+int main(void) {
   int p;
-  int i=1; int N = 3;
+  int i = 1;
+  int N = 3;
   do {
-    p = nfork (i) ;
-    if (p != 0 )
-      printf ("nb fils %d , pid %d\n",p,getpid());
-	} while ((p ==0) && (++i<=N));
-	printf ("FIN %d\n", getpid());
-	return EXIT_SUCCESS;
+    p = fork();
+    if (p != 0)
+      printf("fils %d , pid %d\n", p, getpid());
+  } while ((p == 0) && (++i <= N));
+  printf("FIN %d\n", getpid());
+  return EXIT_SUCCESS;
 }
