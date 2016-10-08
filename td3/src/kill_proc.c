e213b0515cf74e506036fd9abd30606739a9f247
Arib Yassine <aribyassine@hotmail.fr>
2016-10-08 21:55:41 +0200

    kill_proc

---
+++ td3/src/kill_proc.c
@@ -0,0 +1,92 @@
+#define _XOPEN_SOURCE 700
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int num_proc = 1;
+int nb_proc = 0;
+int *pids;
+
+void sig_hand(int sig) { printf("signal reçu %d \n", sig); }
+
+/* La fonction create_process duplique le processus appelant et retourne
+         le PID du processus fils ainsi créé */
+pid_t create_process(void) {
+  pid_t pid;
+  do {
+    pid = fork();
+  } while ((pid == -1) && (errno == EAGAIN));
+  return pid;
+}
+
+/* La fonction child_process effectue les actions du processus fils */
+void child_process(void) {
+  pids[num_proc - 1] = getppid();
+  if (num_proc == nb_proc) {
+    printf(
+        "Une valeur aléatoire entre 0 et 100 : %d\nles pids des processus :\n",
+        (int)(rand() / (((double)RAND_MAX + 1) / 100)));
+    int i = 0;
+    for (i = 0; i < nb_proc; i++) {
+      printf("processus %d => %d | ", i + 1, pids[i]);
+    }
+    printf("processus : %d, pid :%d\n", nb_proc + 1, getpid());
+    kill(getppid(), SIGUSR1);
+  }
+}
+
+/* La fonction father_process effectue les actions du processus père */
+void father_process(int child_pid, sigset_t sig_proc) {
+  sigfillset(&sig_proc);
+  sigdelset(&sig_proc, SIGUSR1);
+  sigsuspend(&sig_proc);
+  printf("\n Nous sommes dans le père %d\n"
+         " Le PID du fils est %d.\n"
+         " Le PID du père est %d.\n",
+         num_proc, (int)child_pid, (int)getpid());
+  if (num_proc != 1) {
+    kill(getppid(), SIGUSR1);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Nombre d'argument incorrect\n");
+    return EXIT_FAILURE;
+  }
+  nb_proc = atoi(argv[1]);
+  pid_t pid;
+  int tab[nb_proc];
+  pids = tab;
+
+  sigset_t sig_proc;
+  struct sigaction action;
+
+  sigemptyset(&sig_proc);
+  action.sa_mask = sig_proc;
+  action.sa_flags = 0;
+  action.sa_handler = sig_hand;
+  sigaction(SIGUSR1, &action, 0);
+
+  do {
+    pid = create_process();
+    switch (pid) {
+    case -1:
+      perror("fork");
+      return EXIT_FAILURE;
+      break;
+    /* Si on est dans le fils */
+    case 0:
+      child_process();
+      break;
+    /* Si on est dans le père */
+    default:
+      father_process(pid, sig_proc);
+      break;
+    }
+  } while ((pid == 0) && (++num_proc <= nb_proc));
+  return EXIT_SUCCESS;
+}
