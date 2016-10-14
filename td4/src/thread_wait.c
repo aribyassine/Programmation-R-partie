881958f0bd19449d050021d5247ce9219e56412c
Arib Yassine <aribyassine@hotmail.fr>
2016-10-14 20:47:05 +0200

    tp4

--- td4/src/thread_wait.c
+++ td4/src/thread_wait.c
@@ -5,33 +5,13 @@
 
 int total;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
 
 void *func_thread(void *arg) {
-  static int nb_appel = 0;
-  pthread_mutex_lock(&mutex); /* On verrouille le mutex */
-  nb_appel++;
   int valeur = (int)(10 * ((double)rand()) / RAND_MAX);
-  printf("tid: %d => valeur aléatoire: %d\n", nb_appel, valeur);
+  printf("valeur aléatoire: %d\n", valeur);
+  pthread_mutex_lock(&mutex); /* On verrouille le mutex */
   total += valeur;
   pthread_mutex_unlock(&mutex); /* On déverrouille le mutex */
-
-  if (nb_appel == 1) {
-    /* On verrouille le mutex */
-    pthread_mutex_lock(&mutex);
-    /* On attend que la condition soit remplie */
-    pthread_cond_wait(&condition, &mutex);
-    /* On déverrouille le mutex */
-    pthread_mutex_unlock(&mutex);
-    printf("total valeurs aléatoires: %d\n", total);
-  } else if (nb_appel == (int)arg) {
-    /* On verrouille le mutex */
-    pthread_mutex_lock(&mutex);
-    /* On délivre le signal : condition remplie */
-    pthread_cond_signal(&condition);
-    /* On déverrouille le mutex */
-    pthread_mutex_unlock(&mutex);
-  }
   pthread_exit(NULL);
 }
 
@@ -45,7 +25,7 @@ int main(int argc, char *argv[]) {
   int i, status[nb_threads];
   pthread_t tid[nb_threads];
   for (i = 0; i < nb_threads; i++) {
-    if (pthread_create(&(tid[i]), NULL, func_thread, (void *)nb_threads) != 0) {
+    if (pthread_create(&(tid[i]), NULL, func_thread, (void *)NULL) != 0) {
       perror("pthread_create");
       return EXIT_FAILURE;
     }
@@ -56,5 +36,6 @@ int main(int argc, char *argv[]) {
       return EXIT_FAILURE;
     }
   }
+  printf("total valeurs aléatoires: %d\n", total);
   return EXIT_SUCCESS;
 }
