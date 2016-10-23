18a1b6f7b5934bd58cabb5a3b90739156e776755
Arib Yassine <aribyassine@hotmail.fr>
2016-10-23 20:59:34 +0200

    td5 exo 1

--- td5/src/lib_upper.c
+++ td5/src/lib_upper.c
@@ -1,31 +1,30 @@
 #define _XOPEN_SOURCE 700
 #define _REENTRANT
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int upper(char *path)
-{
-    FILE* fp1, *fp2;
-    int c = 1;
+int upper(char *path) {
+  FILE *fp1, *fp2;
+  int c = 1;
 
-    fp1= fopen (path, "r");
-    fp2= fopen (path, "r+");
-    if ((fp1== NULL) || (fp2== NULL)) {
-        perror (path);
-	return errno;
-    }
+  fp1 = fopen(path, "r");
+  fp2 = fopen(path, "r+");
+  if ((fp1 == NULL) || (fp2 == NULL)) {
+    perror(path);
+    return errno;
+  }
 
-    while (c !=EOF) {
-        c=fgetc(fp1);
-        if (c!=EOF)
-        fputc(toupper(c),fp2);
-    }
+  while (c != EOF) {
+    c = fgetc(fp1);
+    if (c != EOF)
+      fputc(toupper(c), fp2);
+  }
 
-    fclose (fp1);
-    fclose (fp2);
+  fclose(fp1);
+  fclose(fp2);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
---
+++ td5/src/thread_upper.c
@@ -0,0 +1,40 @@
+#define _XOPEN_SOURCE 700
+#define _REENTRANT
+#include "lib_upper.h"
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *func_thread(void *arg) { pthread_exit((void *)upper((char *)arg)); }
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Nombre d'argument incorrect\n");
+    return EXIT_FAILURE;
+  }
+  int nb_threads = argc - 1;
+  pthread_t tid[nb_threads];
+  int i, status[nb_threads];
+
+  for (i = 0; i < nb_threads; i++) {
+    if (pthread_create(&(tid[i]), NULL, func_thread, (void *)argv[i + 1]) !=
+        0) {
+      perror("pthread_create");
+      return EXIT_FAILURE;
+    }
+  }
+  int nb_error = 0;
+  for (i = 0; i < nb_threads; i++) {
+    if (pthread_join(tid[i], (void **)&status[i]) != 0) {
+      perror("error pthread_join");
+      return EXIT_FAILURE;
+    }
+    if (status[i] != 0) {
+      printf("%s\n", argv[i + 1]);
+      nb_error++;
+    }
+  }
+  return nb_error;
+}
